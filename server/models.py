680025ce0dc515adb4dc79a91d383d61a552db5f
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2026-01-19 07:43:52 +0300

    completed assignment.

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 
+
 metadata = MetaData(
     naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
